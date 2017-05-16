89951c9c7060c018ea413caf5926e3c69f324981
tamilmani <tamilmani@smartglossa>
2017-05-16 15:47:08 +0530

    add file

--- rd/src/main/webapp/js/customer.js
+++ rd/src/main/webapp/js/customer.js
@@ -19,4 +19,48 @@ function cus(){
 	cus += "<\/table>";
 	cus += "<\/div>";
 $('.customer')[0].innerHTML=cus;
-}	
+}
+function cusAcc(){
+	var cusAcc="";
+	cusAcc += "<div>";
+	cusAcc += "<table>";
+	cusAcc += "<tr>";
+	cusAcc += "<td>Aadhar Number<\/td><td><input type=\"text\" id=\"aadhar\"><\/td>";
+	cusAcc += "<\/tr>";
+	cusAcc += "<tr>";
+	cusAcc += "<td>Account Number<\/td><td><input type=\"text\" id=\"acnum\"><\/td>";
+	cusAcc += "<\/tr>";
+	cusAcc += "<td>Amount<\/td><td><input type=\"text\" id=\"amt\"><\/td>";
+	cusAcc += "<\/tr>";
+	cusAcc += "<tr>";
+	cusAcc += "<td>Date<\/td><td><input type=\"date\" id=\"date\"><\/td>";
+	cusAcc += "<\/tr>";
+	cusAcc += "<tr>";
+	cusAcc += "<td>Duration<\/td><td><input type=\"text\" id=\"duration\"><\/td>";
+	cusAcc += "<\/tr>";
+	cusAcc += "<tr>";
+	cusAcc += "<td><button id=\"add\">Add<\/button><\/td>";
+	cusAcc += "<\/tr>";
+	cusAcc += "<\/table>";
+	cusAcc += "<\/div>";
+$('.customer')[0].innerHTML=cusAcc;
+}
+function payment(){
+	var payment="";
+	payment += "<div>";
+	payment += "<table>";
+	payment += "<tr>";
+	payment += "<td>Account Number<\/td><td><input type=\"text\" id=\"acnum\"><\/td>";
+	payment += "<\/tr>";
+	payment += "<tr>";
+	payment += "<td>Amount<\/td><td><input type=\"text\" id=\"amt\"><\/td>";
+	payment += "<\/tr>";
+	payment += "<td>Date<\/td><td><input type=\"date\" id=\"date\"><\/td>";
+	payment += "<\/tr>";
+	payment += "<tr>";
+	payment += "<td><button id=\"add\">Add<\/button><\/td>";
+	payment += "<\/tr>";
+	payment += "<\/table>";
+	payment += "<\/div>";
+$('.customer')[0].innerHTML=payment;
+}
--- rd/src/main/webapp/js/rd.js
+++ rd/src/main/webapp/js/rd.js
@@ -63,6 +63,68 @@ $(document).ready(function(){
 			alert("Error Accours");
 		})
 	});
+	$(document).on("click","#add",function(){
+		var aadhar=$("#aadhar").val();
+		var acnum=$("#acnum").val();
+		var amt=$("#amt").val();
+		var date=$("#date").val();
+		var duration=$("#duration").val();
+		if(aadhar==""){
+			$("#aadhar").focus().css("outline-color","red");
+			return false;
+		}
+		if(acnum==""){
+			$("#acnum").focus().css("outline-color","red");
+			return false;
+		}
+		if(amt==""){
+			$("#amt").focus().css("outline-color","red");
+			return false;
+		}
+		if(date==""){
+			$("#date").focus().css("outline-color","red");
+			return false;
+		}
+		if(duration==""){
+			$("#duration").focus().css("outline-color","red");
+			return false;
+		}
+		var url="/rd/RdServlet?operation=addCusAccount&aadharNum="+aadhar+"&accNum="+acnum+"&amount="+amt+"&date="+date+"&duration="+duration; 
+		$.ajax({
+			url:url,
+			type:'POST'
+		}).done(function(result){
+			alert("Successfully Added");
+		}).fail(function(result){
+			alert("Please Check Deatils");
+		})
+	});
+	$(document).on("click","#add",function(){
+		var acnum=$("#acnum").val();
+		var amt=$("#amt").val();
+		var date=$("#date").val();
+		if(acnum==""){
+			$("#acnum").focus().css("outline-color","red");
+			return false;
+		}
+		if(amt==""){
+			$("#amt").focus().css("outline-color","red");
+			return false;
+		}
+		if(date==""){
+			$("#date").focus().css("outline-color","red");
+			return false;
+		}
+		var url="/rd/RdServlet?operation=addPayment&accNum="+acnum+"&amount="+amt+"&date="+date;
+		$.ajax({
+			url:url,
+			type:'POST'
+		}).done(function(result){
+			alert("Successfully Added");
+		}).fail(function(result){
+			alert("Please Check Deatils");
+		})
+	});
 	
 	
 	
